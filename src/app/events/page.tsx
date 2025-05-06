9ec602c87af1f497962aadd230eee0ad70668afb
Tina Huynh <tinamchuynh@gmail.com>
2025-05-05 21:11:07 -0700

    feat: add pages for Gallery, About Us, Contact Us, Events, Menu, Reservations, and Private Dining

---
+++ src/app/events/page.tsx
@@ -0,0 +1,13 @@
+export default function EventsPage() {
+  return (
+    <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
+      <div className="relative px-6 lg:px-8 max-w-7xl">
+        <div>
+          <h2 className="font-semibold text-gray-900 text-lg leading-8">
+            Events
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,13 @@
+export default function GalleryPage() {
+  return (
+    <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
+      <div className="relative">
+        <div>
+          <h2 className="font-semibold text-gray-900 text-lg leading-8">
+            Gallery
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
