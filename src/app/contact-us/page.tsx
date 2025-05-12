26f809ab32e378a30d9ffc3ad99b3feae26c883c
Tina Huynh <tinamchuynh@gmail.com>
2025-05-11 20:26:11 -0700

    fix: remove unnecessary classes from multiple page components

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,6 +1,6 @@
 export default function ContactUsPage() {
   return (
-    <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
+    <div>
       <div className="relative px-6 lg:px-8 max-w-7xl">
         <div>
           <h2 className="font-semibold text-gray-900 text-lg leading-8">
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,6 +1,6 @@
 export default function GalleryPage() {
   return (
-    <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
+    <div>
       <div className="relative">
         <div>
           <h2 className="font-semibold text-gray-900 text-lg leading-8">
