df68cfe2983537d60e26fa07ab2a2beba33ed72b
Tina Huynh <tinamchuynh@gmail.com>
2025-05-11 23:10:23 -0700

    Simplifies header in Contact Us page
    Removes unnecessary styling from the header element.
    
    Streamlines the user interface for a cleaner look.

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -3,9 +3,7 @@ export default function ContactUsPage() {
     <div>
       <div className="relative px-6 lg:px-8 max-w-7xl">
         <div>
-          <h2 className="font-semibold text-gray-900 text-lg leading-8">
-            Contact Us
-          </h2>
+          <h2>Contact Us</h2>
         </div>
       </div>
     </div>
