7a78f89a81faaa3284d0f68b0188f284435d8a2c
Tina Huynh <tinamchuynh@gmail.com>
2025-05-11 23:10:18 -0700

    fix: simplify About Us heading by removing unnecessary classes

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,9 +3,7 @@ export default function AboutUsPage() {
     <div>
       <div className="relative px-6 lg:px-8 max-w-7xl">
         <div>
-          <h2 className="font-semibold text-gray-900 text-lg leading-8">
-            About Us
-          </h2>
+          <h2>About Us</h2>
         </div>
       </div>
     </div>
