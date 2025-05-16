6e42d51a9685033d1f8d3ec4643a9bc294279acf
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 16:54:09 -0700

    feat: add seasonal and popular drinks/food carousels with descriptions and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselApi,
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
+import {
+  popularDrinks,
+  popularFood,
+  seasonalDrinksCarouselImages,
+  seasonalFoodCarouselImages,
+} from "@/lib/constants/images/carousels";
 import Autoplay from "embla-carousel-autoplay";
-import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/image";
 import { useCallback, useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 export default function HomePage() {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
   const [api, setApi] = useState<CarouselApi>();
 
   const scrollPrev = useCallback(() => {
@@ -43,6 +46,21 @@ export default function HomePage() {
         />
       </section>
       <section className="relative mx-auto px-4 pt-24 pb-16 max-w-7xl">
+        <h1>Our Seasonal Drinks</h1>
+        <p>
+          Our seasonal drinks are a delightful celebration of flavors and
+          ingredients that change with the seasons. From refreshing summer
+          smoothies to cozy winter lattes, our menu evolves to capture the
+          essence of each time of year. Each drink is crafted with care,
+          showcasing the freshest ingredients and unique flavor combinations.
+          Join us in savoring our seasonal drinks, where every sip is a
+          delightful experience that reflects the beauty of nature's bounty.
+          Whether you're looking for a refreshing pick-me-up or a comforting
+          indulgence, our seasonal drinks are designed to tantalize your taste
+          buds and elevate your experience. Discover the magic of the seasons
+          through our carefully curated selection of beverages, where every sip
+          is a celebration of flavor and creativity.
+        </p>
         <Carousel
           setApi={setApi}
           opts={{
@@ -51,26 +69,213 @@ export default function HomePage() {
           }}
           plugins={[
             Autoplay({
-              delay: 9000,
+              delay: 3500,
             }),
           ]}
-          className="mx-auto w-10/12 md:w-11/12 lg:w-full"
+          className="mx-auto mt-7 w-10/12 md:w-11/12 lg:w-full"
         >
           <CarouselContent className="-ml-1">
-            {Array.from({ length: 5 }).map((_, index) => (
+            {seasonalDrinksCarouselImages.map((image, index) => (
               <CarouselItem
                 key={index}
                 className="pl-1 md:basis-1/2 lg:basis-1/3"
               >
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex justify-center items-center p-6 aspect-square">
-                      <span className="font-semibold text-2xl">
-                        {index + 1}
-                      </span>
-                    </CardContent>
-                  </Card>
-                </div>
+                <Image
+                  src={image}
+                  alt=""
+                  className="rounded-lg h-full object-cover"
+                  width={400}
+                  height={400}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+
+        <div className="flex justify-end gap-4 mx-auto my-3 lg:my-10 w-10/12 md:w-11/12">
+          <Button
+            size={"icon"}
+            variant={"icon"}
+            className="flex justify-center items-center rounded-full"
+            onClick={scrollPrev}
+          >
+            <FaChevronLeft />
+          </Button>
+          <Button
+            size={"icon"}
+            variant={"icon"}
+            className="rounded-full"
+            onClick={scrollNext}
+          >
+            <FaChevronRight />
+          </Button>
+        </div>
+      </section>
+
+      <section className="relative mx-auto px-4 pt-24 pb-16 max-w-7xl">
+        <h1>Our Seasonal Food Items</h1>
+        <p>
+          Our seasonal food items are a delightful fusion of flavors and
+          ingredients that celebrate the essence of each season. From hearty
+          soups to vibrant salads, our menu changes with the seasons to ensure
+          you experience the freshest and most delicious offerings. Whether
+          you're looking for a light summer dish or a comforting winter meal,
+          our seasonal food items are crafted with care and creativity. Join us
+          in savoring the flavors of the season, where every bite is a
+          celebration of nature's bounty. Indulge in our seasonal food items and
+          discover a world of culinary delights that will tantalize your taste
+          buds and leave you craving more.
+        </p>
+        <Carousel
+          setApi={setApi}
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 3000,
+            }),
+          ]}
+          className="mx-auto mt-7 w-10/12 md:w-11/12 lg:w-full"
+        >
+          <CarouselContent className="-ml-1">
+            {seasonalFoodCarouselImages.map((image, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 md:basis-1/2 lg:basis-1/3"
+              >
+                <Image
+                  src={image}
+                  alt=""
+                  className="rounded-lg h-full object-cover"
+                  width={400}
+                  height={400}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+
+        <div className="flex justify-end gap-4 mx-auto my-3 lg:my-10 w-10/12 md:w-11/12">
+          <Button
+            size={"icon"}
+            variant={"icon"}
+            className="flex justify-center items-center rounded-full"
+            onClick={scrollPrev}
+          >
+            <FaChevronLeft />
+          </Button>
+          <Button
+            size={"icon"}
+            variant={"icon"}
+            className="rounded-full"
+            onClick={scrollNext}
+          >
+            <FaChevronRight />
+          </Button>
+        </div>
+      </section>
+
+      <section className="relative mx-auto px-4 pt-24 pb-16 max-w-7xl">
+        <h1>Our Popular Drinks</h1>
+        <p>
+          Our popular drinks are a testament to our commitment to quality and
+          flavor. From classic favorites to innovative creations, each drink is
+          crafted with the finest ingredients and utmost care. Whether you're in
+          the mood for a rich espresso, a refreshing iced tea, or a decadent
+          smoothie, our menu has something to satisfy every palate. Join us in
+          exploring our most beloved beverages, where every sip is a delightful
+          experience that keeps our customers coming back for more.
+        </p>
+        <Carousel
+          setApi={setApi}
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 2850,
+            }),
+          ]}
+          className="mx-auto mt-7 w-10/12 md:w-11/12 lg:w-full"
+        >
+          <CarouselContent className="-ml-1">
+            {popularDrinks.map((image, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 md:basis-1/2 lg:basis-1/3"
+              >
+                <Image
+                  src={image}
+                  alt=""
+                  className="rounded-lg h-full object-cover"
+                  width={400}
+                  height={400}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+
+        <div className="flex justify-end gap-4 mx-auto my-3 lg:my-10 w-10/12 md:w-11/12">
+          <Button
+            size={"icon"}
+            variant={"icon"}
+            className="flex justify-center items-center rounded-full"
+            onClick={scrollPrev}
+          >
+            <FaChevronLeft />
+          </Button>
+          <Button
+            size={"icon"}
+            variant={"icon"}
+            className="rounded-full"
+            onClick={scrollNext}
+          >
+            <FaChevronRight />
+          </Button>
+        </div>
+      </section>
+
+      <section className="relative mx-auto px-4 pt-24 pb-16 max-w-7xl">
+        <h1>Our Popular Food Items</h1>
+        <p>
+          Our popular food items are a celebration of flavor and creativity,
+          crafted to satisfy every craving. From savory sandwiches to delectable
+          pastries, our menu features a diverse range of dishes that have
+          captured the hearts of our customers. Each item is made with the
+          freshest ingredients and a passion for culinary excellence. Join us in
+          indulging in our most beloved food offerings, where every bite is a
+          delightful experience that keeps our guests coming back for more.
+        </p>
+        <Carousel
+          setApi={setApi}
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 2700,
+            }),
+          ]}
+          className="mx-auto mt-7 w-10/12 md:w-11/12 lg:w-full"
+        >
+          <CarouselContent className="-ml-1">
+            {popularFood.map((image, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 md:basis-1/2 lg:basis-1/3"
+              >
+                <Image
+                  src={image}
+                  alt=""
+                  className="rounded-lg h-full object-cover"
+                  width={400}
+                  height={400}
+                />
               </CarouselItem>
             ))}
           </CarouselContent>
---
+++ src/lib/constants/images/carousels.ts
@@ -0,0 +1,50 @@
+export const seasonalDrinksCarouselImages = [
+  "https://media.istockphoto.com/id/1066197954/photo/sweet-peppermint-iced-coffee.jpg?s=612x612&w=0&k=20&c=yrSEcgX0UQGergLRUk6qEofbGDIrrN0aSjurQ-U7UyY=",
+  "https://media.istockphoto.com/id/493694816/photo/festive-pumpkin-latte-and-almond-biscuits-top-view.jpg?s=612x612&w=0&k=20&c=Of0IRne1s2vqPfkfks11H374EiJPYa5Yw8VIn5aNSC8=",
+  "https://media.istockphoto.com/id/1402518417/photo/cappuccino-or-latte-with-milk-foam-and-caramel-in-a-glass-with-coffee-beans-on-light-marble.jpg?s=612x612&w=0&k=20&c=TkODCXLPzxy6MGSWxExbuwAucgbgv9opqkZ0R8FaMec=",
+  "https://media.istockphoto.com/id/1456383785/photo/nutella-chocolate-milkshake-with-whipped-cream-in-mason-jar.jpg?s=612x612&w=0&k=20&c=lmdvAjUZN5Cf4KnQRy-COG7Oqw6Rw-0bpzUHijrgQg4=",
+  "https://media.istockphoto.com/id/1001586996/photo/cup-of-cocoa-with-cinnamon-whipped-cream-and-chocolate-textile-background-scandinavian-winter.jpg?s=612x612&w=0&k=20&c=C7ZIB38dje7_kZhsPgpVuvVIG_badjV4QU02Ul-Cans=",
+  "https://media.istockphoto.com/id/1401392907/photo/iced-latte-on-wooden-table.jpg?s=612x612&w=0&k=20&c=BVcg-MXRYJTylRbyvEI82xX_-SdzTAsK3S-ikBhZuho=",
+  "https://media.istockphoto.com/id/1277341357/photo/hot-chocolate.jpg?s=612x612&w=0&k=20&c=ulE1iwpi6QQlyo6UOsp5bd0w0KCHWvDqYL_uZr51nGU=",
+  "https://media.istockphoto.com/id/1714764916/photo/coffee-cup-of-cappuccino-with-lavender-flowers-cozy-breakfast.jpg?s=612x612&w=0&k=20&c=8nYSP0xgWfrZrsTUdDsF78deLY5Gwswu6kQ7c2rDC_Y=",
+  "https://media.istockphoto.com/id/1468115811/photo/cinnamon-roll-with-chocolate-milkshake.jpg?s=612x612&w=0&k=20&c=YurGS8h6bbi48Eh1MBk4SfGUxyZggXyys0RfOvOi3eo=",
+];
+
+export const seasonalFoodCarouselImages = [
+  "https://media.istockphoto.com/id/2164665781/photo/one-slice-piece-of-pecan-pie-with-whipped-cream.jpg?s=612x612&w=0&k=20&c=JUeuHn7IceyD26Z8qM6ZgXN7UCVAXlBSjC61RxEdTDY=",
+  "https://media.istockphoto.com/id/1284622401/photo/christmas-cookies-on-plate-top-view.jpg?s=612x612&w=0&k=20&c=rJK8cg8etjDrqEN9BOQhsWmd6sDv5SAksht-cl-AVfs=",
+  "https://media.istockphoto.com/id/1449381641/photo/sweet-crumble-with-truffle-strawberry-blueberry-and-ice-cream.jpg?s=612x612&w=0&k=20&c=Oau1MCCm63vKEZyieGWP3rCyjo3_wJ-AuW0OplLBdFk=",
+  "https://media.istockphoto.com/id/1818662857/photo/apple-cider-donuts.jpg?s=612x612&w=0&k=20&c=P1mb3vGVTodA2C4j0DAWPH6N_Pk34u4iy41T-9v-6RE=",
+  "https://media.istockphoto.com/id/531138503/photo/popcorn.jpg?s=612x612&w=0&k=20&c=_E_BY72mgkn885pXkCWMwHgWSvEc4U6BEY6jxcf-rp0=",
+];
+
+export const popularDrinks = [
+  "https://media.istockphoto.com/id/1300870779/photo/a-glass-of-coffee-with-ice-milk-and-lavender-flowers-on-a-light-background-cold-summer-drink.jpg?s=612x612&w=0&k=20&c=df5ymrvMugjLl9S3D-V-a3BBFB85XoVNSkPkGmlJ-iY=",
+  "https://media.istockphoto.com/id/2212821081/photo/barista-pouring-cream-into-cold-brew-coffee.jpg?s=612x612&w=0&k=20&c=z7cPT5dHEUW4YOhoKAPtmbheIi9p8IAZ0saiBgoQs6k=",
+  "https://media.istockphoto.com/id/1598170144/photo/iced-coffee-latte-in-a-tall-glass-with-milk-on-a-light-background-with-coffee-beans-spoon-and.jpg?s=612x612&w=0&k=20&c=qEyticKJbocni5tuX_WGCNpLuc0bax7ucGvNhma66Sk=",
+  "https://media.istockphoto.com/id/1320860167/photo/iced-matcha-latte-green-tea-with-almond-milk-traditional-matcha-tools-with-bamboo-straw-in.jpg?s=612x612&w=0&k=20&c=BXzpQ8ypbiWpYZyGvU_rXSVvLttSDgat9Eox8sNw3GU=",
+  "https://media.istockphoto.com/id/2149749083/photo/iced-cappuccino-caramel-with-creamy-float.jpg?s=612x612&w=0&k=20&c=kcZa8D2cmHfx-pUKIpi3PSkbOGaIEdBDPvSU6Ckti1Q=",
+  "https://media.istockphoto.com/id/1452577726/photo/fresh-cold-coffee-with-milk-and-whipped-cream.jpg?s=612x612&w=0&k=20&c=RNvjU9QX1SE5rwB-qz6QtP0KpSk33CV31C1MlzXYYlQ=",
+  "https://media.istockphoto.com/id/1285147950/photo/iced-pink-matcha-with-milk-in-tall-glass.jpg?s=612x612&w=0&k=20&c=K_N-C_j0b7dA6EMbHIeh1Dq8rH5AMr_IxwSjMUIBbeg=",
+  "https://media.istockphoto.com/id/990984520/photo/ice-coffee-on-table.jpg?s=612x612&w=0&k=20&c=D_eQYUoXfXS7OSVHDnoTo66H91MKaDgQQOQUXBWrOPk=",
+  "https://media.istockphoto.com/id/1054146708/photo/iced-matcha-latte-with-milk-pouring-fron-pitcher-in-glass.jpg?s=612x612&w=0&k=20&c=uxHEqGPp_amnH8ctne7gCjX3udPqOSvqeHMuFdAbipc=",
+  "https://media.istockphoto.com/id/1401392907/photo/iced-latte-on-wooden-table.jpg?s=612x612&w=0&k=20&c=BVcg-MXRYJTylRbyvEI82xX_-SdzTAsK3S-ikBhZuho=",
+  "https://media.istockphoto.com/id/1315419108/photo/freshly-prepared-dalgona-coffee.jpg?s=612x612&w=0&k=20&c=9nZ6zsNV7quS7VyaSnkGMsjN-f_mZyiqwGYPLm601o8=",
+  "https://media.istockphoto.com/id/2200792535/photo/glass-of-iced-coffee-with-milk-and-espresso-swirl-topped-with-ice-cubes-and-a-black-straw.jpg?s=612x612&w=0&k=20&c=U-Y9oa5gLH2g9WuU-Q-8n1RVnHGL7voUSt6gMDKs7Ns=",
+];
+
+export const popularFood = [
+  "https://media.istockphoto.com/id/511742246/photo/bagel-with-cream-cheese.jpg?s=612x612&w=0&k=20&c=LgfJV2cI0uy7n0X1F9cp52_ouJCqvMvl1xRQgmFVRL0=",
+  "https://media.istockphoto.com/id/1179046614/photo/cup-of-coffee-and-sweet-cakes-with-berries.jpg?s=612x612&w=0&k=20&c=0C6v85YcksZvqaIlWZplh5fVb-yW6ueiEto3kigqg9s=",
+  "https://media.istockphoto.com/id/889126708/photo/strawberry-perfect.jpg?s=612x612&w=0&k=20&c=veaAu_HKcY2i2XUq1UQVtcNCmC_fhO8vawCjUuEEuBU=",
+  "https://media.istockphoto.com/id/2182321684/photo/chocolate-chip-cookie-cookie-biscuit-chocolate-cookie-delicious-chocolate-chip-cookies.jpg?s=612x612&w=0&k=20&c=L__5AvpMPKfHkupxhUfaoF2LAMMAV06bGN4ObSO2ObU=",
+  "https://media.istockphoto.com/id/1615993303/photo/croissants-on-a-grid-in-bakery-filled-with-cream-chocolate-topping-and-almond-flakes.jpg?s=612x612&w=0&k=20&c=6hwnWOX0ppWYLgW2W5usgwgJIFVytJrYPuRqwtn5KCc=",
+  "https://media.istockphoto.com/id/178472102/photo/fancy-panna-cotta-with-strawberries.jpg?s=612x612&w=0&k=20&c=kuD8DdG5ieyvREP9PoB6ClLLW0o3-tQfFv70ZD4S6zc=",
+  "https://media.istockphoto.com/id/1727509923/photo/grilled-cheese-sandwich-on-a-woodem-rustic-background.jpg?s=612x612&w=0&k=20&c=RK8yGxOczVfECjOEHAyR8gzk3YRptUhfx-2KXFnK4XY=",
+  "https://media.istockphoto.com/id/1405973450/photo/cinnamon-buns.jpg?s=612x612&w=0&k=20&c=8jKhRd2QEiJMr1nESdu8mp9Oa5ND4_qcTOwzhEqT8Ys=",
+  "https://media.istockphoto.com/id/1802471046/photo/a-tray-of-caramel-apples-sprinkled-with-nuts-sit-on-display-at-a-market-in-autumn.jpg?s=612x612&w=0&k=20&c=yOZkg6jIkROoqOoM48ICVXp4NMYlMiLrJclGEq_wWBo=",
+  "https://media.istockphoto.com/id/1298159858/photo/acai-smoothie-bowl-with-toppings.jpg?s=612x612&w=0&k=20&c=V3chwb61pb71NZ1erUtHAYwnTrL1Ra6GpAfjkY1pru4=",
+  "https://media.istockphoto.com/id/1978764573/photo/chocolate-dipped-pretzel-rods-on-a-plate.jpg?s=612x612&w=0&k=20&c=IBGvNzoIdKzRO_LNu4ZgwP0sx08fmIGOwpUGHof5OG8=",
+  "https://media.istockphoto.com/id/538690851/photo/homemade-chocolate-dipped-strawberries.jpg?s=612x612&w=0&k=20&c=HbOKyEWdNwHtAYCdEhfPTAOvCZ79xRthCMuWbby_Qnc=",
+  "https://media.istockphoto.com/id/2187916113/photo/italian-dessert-tiramisu-made-of-ladyfingers-dipped-in-coffee-and-layered-with-cream-made-of.jpg?s=612x612&w=0&k=20&c=GnycY2GRE4vFW_sDSJ-VP4ai2BS7072ZTG9P_6lTrKY=",
+];
