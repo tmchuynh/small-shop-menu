ae2f5ead79a8f5ce813e4a93c719eb51cf9018be
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 00:43:28 -0700

    feat: add gallery images and tabbed image structure for enhanced visual content

---
+++ src/lib/constants/images/gallery.ts
@@ -0,0 +1,65 @@
+export const masonryGalleryImages1 = [
+  "https://cdn.pixabay.com/photo/2022/11/07/04/36/vietnamese-hot-honey-eggs-coffee-7575586_1280.jpg",
+  "https://cdn.pixabay.com/photo/2022/11/07/04/58/coconut-7575606_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/18/18/10/architectural-1836161_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/23/00/24/coffee-1851431_1280.jpg",
+  "https://cdn.pixabay.com/photo/2017/08/06/07/09/coffee-2589756_1280.jpg",
+  "https://cdn.pixabay.com/photo/2018/10/19/16/46/coffee-3759012_1280.jpg",
+  "https://cdn.pixabay.com/photo/2017/06/15/19/37/coffee-2406443_1280.jpg",
+  "https://cdn.pixabay.com/photo/2017/07/31/11/42/black-2557553_1280.jpg",
+  "https://cdn.pixabay.com/photo/2025/05/14/09/56/artisan-coffee-9599155_1280.jpg",
+  "https://cdn.pixabay.com/photo/2025/03/19/09/14/ai-generated-9480246_1280.jpg",
+  "https://cdn.pixabay.com/photo/2014/12/11/02/57/coffee-563800_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/29/02/10/caffeine-1866758_1280.jpg",
+];
+
+export const masonryGalleryImages2 = [
+  "https://cdn.pixabay.com/photo/2015/07/12/14/26/coffee-842020_1280.jpg",
+  "https://cdn.pixabay.com/photo/2023/01/13/17/26/macarons-7716580_1280.jpg",
+  "https://cdn.pixabay.com/photo/2015/08/25/06/38/cake-906267_1280.jpg",
+  "https://cdn.pixabay.com/photo/2020/04/22/22/08/cafe-5080266_1280.jpg",
+  "https://cdn.pixabay.com/photo/2020/03/12/16/10/coffee-4925525_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/29/09/00/doughnuts-1868573_1280.jpg",
+  "https://cdn.pixabay.com/photo/2019/07/28/05/00/sweets-4367939_1280.jpg",
+  "https://cdn.pixabay.com/photo/2021/02/17/11/57/coffee-6023991_1280.jpg",
+  "https://cdn.pixabay.com/photo/2025/05/14/09/56/artisan-coffee-9599155_1280.jpg",
+  "https://cdn.pixabay.com/photo/2019/07/28/04/15/sweets-4367899_1280.jpg",
+  "https://cdn.pixabay.com/photo/2014/12/11/02/57/coffee-563800_1280.jpg",
+  "https://cdn.pixabay.com/photo/2019/03/13/16/41/coffee-4053245_1280.jpg",
+];
+
+export const tabbedImages = [
+  {
+    label: "Exterior",
+    value: "exterior",
+    images: [
+      "https://media.istockphoto.com/id/2183246664/photo/modern-glass-facade-of-contemporary-office-building-with-outdoor-seating.jpg?s=612x612&w=0&k=20&c=Zu0GouRI7wTfneXkqW9UE8yXM0Xb3ckzWNot-O1z2Bc=",
+      "https://media.istockphoto.com/id/2183247162/photo/modern-outdoor-patio-with-greenery-and-contemporary-design-elements.jpg?s=612x612&w=0&k=20&c=J2XJz8JIA7x0u4RepeKHf__qU7mCM6NvmpwJkQHoSZU=",
+    ],
+  },
+  {
+    label: "Interior",
+    value: "interior",
+    images: [
+      "https://media.istockphoto.com/id/2149830144/photo/3d-render-of-modern-cafe-restaurant.jpg?s=612x612&w=0&k=20&c=lrRcBQZx49_uyZe_zEKXfO54hxqwH7a3pExwP8mqQqE=",
+      "https://media.istockphoto.com/id/1491595178/photo/vintage-style-coffee-shop-interior-3d-rendering.jpg?s=612x612&w=0&k=20&c=y-H5OhxuG0Qd1j0isgQKmTGU9p3k91ClwTrkHmhKHCM=",
+      "https://media.istockphoto.com/id/1143934437/photo/3d-render-of-cafetaria-and-restaurant.jpg?s=612x612&w=0&k=20&c=W8lHiQg_ujRmhK7GWGe4N1ozhV6ZjeN2fFMxT_7uXlk=",
+      "https://media.istockphoto.com/id/1490331569/photo/coffee-shop-interior-3d-rendering.jpg?s=612x612&w=0&k=20&c=a3xzME0KcHGd28Gl3ADOp5Jd-d43hHlYn3GSmG6Nh8M=",
+    ],
+  },
+  {
+    label: "Behind the Scenes",
+    value: "behind-the-scenes",
+    images: [
+      "https://media.istockphoto.com/id/1339743849/photo/over-the-shoulder-asian-chinese-male-barista-pouring-froth-milk-on-coffee-cup-prepared-coffee.jpg?s=612x612&w=0&k=20&c=QQkfYycgXGgICpF8a4tETUaTGEj1Z-n8aSaFrQa52lU=",
+      "https://media.istockphoto.com/id/1366672341/photo/glass-with-ice-and-coffee.jpg?s=612x612&w=0&k=20&c=lR7zj__gNDmO0aXfjV8N9Ti-WnOx2iuNxRRyEDvFCi4=",
+      "https://media.istockphoto.com/id/856004690/photo/making-coffee.jpg?s=612x612&w=0&k=20&c=x43qd-aYQHh4cppefEEoznG5g5qGnNuGx1h-qE8CW1o=",
+      "https://media.istockphoto.com/id/1030882758/photo/espresso-coffee-and-machine.jpg?s=612x612&w=0&k=20&c=k6ODdihU-Q7zG3ftft-pVYIkBf_V5I8V98UQDG3ChzE=",
+      "https://media.istockphoto.com/id/626522696/photo/making-of-cafe-latte-art.jpg?s=612x612&w=0&k=20&c=NHJxYrjzuuAMLLmmb813EC5H8wPerDgoYAuldPyyuTw=",
+      "https://media.istockphoto.com/id/905029034/photo/coffee-dripping-from-the-machine-into-the-cup.jpg?s=612x612&w=0&k=20&c=YwLnvQsss1Q4QFTSxknGxelO-MCYAc32osP8qPFwG-o=",
+      "https://media.istockphoto.com/id/1342611877/photo/close-up-of-hand-pouring-coffee-latte-art.jpg?s=612x612&w=0&k=20&c=lhkQmJDW1DhiiVyB1rNvCvdwTlA-M-t_skexAIPXxc4=",
+      "https://media.istockphoto.com/id/1294131797/photo/fresh-ground-coffee-close-up-back-lit-background.jpg?s=612x612&w=0&k=20&c=OhN64dcZ65RfEhGI7VegSeDRfMyqRyfEqJoNRUTDC7M=",
+      "https://media.istockphoto.com/id/1144838255/photo/barista-pours-milk-into-cold-brew-coffee.jpg?s=612x612&w=0&k=20&c=rGNAA8ZF1cAEbQbfF0kcEDHx_icXXY7iPXFLzPgSrLs=",
+    ],
+  },
+];
