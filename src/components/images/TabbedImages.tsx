0f39a67a515ee95bbb1ddee25524daea45846342
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 00:43:39 -0700

    feat: refactor TabbedImages component for improved layout and hover effects

--- src/components/images/TabbedImages.tsx
+++ src/components/images/TabbedImages.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { AnimatePresence, motion } from "motion/react";
 import Image from "next/image";
+import { useState } from "react";
 
 export function TabbedImages({
   data,
 }: {
-  data: { label: string; value: string; images: { imageLink: string }[] }[];
+  data: { label: string; value: string; images: string[] }[];
 }) {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
   return (
-    <Tabs value="html">
+    <Tabs defaultValue={data[0].value} className="w-full">
       <TabsList>
         {data.map(({ label, value }) => (
           <TabsTrigger key={value} value={value}>
@@ -15,27 +21,47 @@ export function TabbedImages({
           </TabsTrigger>
         ))}
       </TabsList>
-      <div className="gap-4 grid grid-cols-1">
-        {data.map(({ value, images }) => (
-          <TabsContent
-            value={value}
-            className="gap-4 grid grid-cols-2 md:grid-cols-3"
-            key={value}
-          >
-            {images.map(({ imageLink }, index) => (
-              <div key={index}>
-                <Image
-                  className="rounded-lg w-full max-w-full h-40 object-cover object-center"
-                  src={imageLink}
-                  alt="image-photo"
-                  width={500}
-                  height={500}
-                />
-              </div>
-            ))}
-          </TabsContent>
-        ))}
-      </div>
+      {data.map(({ value, images }) => (
+        <TabsContent
+          value={value}
+          className="gap-2 grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-6 xl:grid-cols-4"
+          key={value}
+        >
+          {images.map((src, index) => (
+            <div
+              key={index}
+              className="group block relative p-2 w-full h-full"
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <AnimatePresence>
+                {hoveredIndex === index && (
+                  <motion.span
+                    className="block z-10 absolute inset-0 bg-neutral-200 dark:bg-slate-800/[0.8] rounded-3xl w-full"
+                    layoutId="hoverBackground"
+                    initial={{ opacity: 0 }}
+                    animate={{
+                      opacity: 1,
+                      transition: { duration: 0.35 },
+                    }}
+                    exit={{
+                      opacity: 0,
+                      transition: { duration: 0.25, delay: 0.4 },
+                    }}
+                  />
+                )}
+              </AnimatePresence>
+              <Image
+                className="relative z-50 rounded-2xl w-full max-w-full h-40 overflow-hidden object-cover object-center"
+                src={src}
+                alt="image-photo"
+                width={500}
+                height={500}
+              />
+            </div>
+          ))}
+        </TabsContent>
+      ))}
     </Tabs>
   );
 }
