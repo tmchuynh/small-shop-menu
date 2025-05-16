55b1b3b7419216133e049e5f964971956ef872d0
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 14:17:31 -0700

    fix: update drink images in drinkMenu for improved visual appeal

--- src/lib/constants/menu/drinkMenu.ts
+++ src/lib/constants/menu/drinkMenu.ts
@@ -343,19 +343,22 @@ export const drinkMenu: MenuCategory[] = [
       {
         name: "Nitro Cold Brew",
         description: "A smooth and creamy cold brew infused with nitrogen.",
-        image: "/images/nitro-cold-brew.jpg",
+        image:
+          "https://cdn.pixabay.com/photo/2022/11/06/18/56/coffee-7574762_1280.jpg",
         price: 4.0,
       },
       {
         name: "Vanilla Sweet Cream Cold Brew",
         description: "A refreshing cold brew with vanilla sweet cream.",
-        image: "/images/vanilla-sweet-cream-cold-brew.jpg",
+        image:
+          "https://media.istockphoto.com/id/2151934434/photo/refreshing-cold-foam-cold-brew-coffee.jpg?s=612x612&w=0&k=20&c=sxPm6uh0l0pXr6dOZ53mBovCC-PR0oBKRki-O4722gA=",
         price: 4.5,
       },
       {
         name: "Cold Brew with Pistachio Cream Milk",
         description: "A smooth cold brew with milk.",
-        image: "/images/cold-brew-with-milk.jpg",
+        image:
+          "https://media.istockphoto.com/id/1145089586/photo/fresh-cold-brew-coffee.jpg?s=612x612&w=0&k=20&c=aEVpsLTBJ9JXoJprcoeVOtfIAh4_fT4ZWE6wCieaZts=",
         price: 4.0,
       },
     ],
@@ -367,63 +370,73 @@ export const drinkMenu: MenuCategory[] = [
       {
         name: "Strawberry Açaí",
         description: "A refreshing drink with strawberry and açaí flavor.",
-        image: "/images/strawberry-acai-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/1402110089/photo/strawberry-slush-drink-with-fruit-and-ice-and-isolated-background.jpg?s=612x612&w=0&k=20&c=I-VvYsd4xBP4I1dt_LYlbk8G3YkQ2D1jQMyC_1sqQb0=",
         price: 4.0,
       },
       {
         name: "Mango Dragonfruit",
         description: "A tropical drink with mango and dragonfruit flavor.",
-        image: "/images/mango-dragonfruit-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/1334662028/photo/a-glass-of-dragon-fruit-cocktail-on-a-pink-background-cool-and-icy-summer-sweet-drink.jpg?s=612x612&w=0&k=20&c=Vy8lv0BOkdkwb8HdauLzdzbg46mdtVS5HnXvuCAhn3g=",
         price: 4.0,
       },
       {
         name: "Pink Drink",
         description: "A creamy drink with coconut milk and strawberries.",
-        image: "/images/pink-drink.jpg",
+        image:
+          "https://media.istockphoto.com/id/975789524/photo/raspberry-mojito-lemonade-with-lime-and-fresh-mint-in-glass-on-wooden-background.jpg?s=612x612&w=0&k=20&c=townWwRffiGBQlCYkV7QztpoGgA4GBoQNgERhViyRfg=",
         price: 4.0,
       },
       {
         name: "Pineapple Passionfruit",
         description: "A tropical drink with pineapple and passionfruit flavor.",
-        image: "/images/pineapple-passionfruit-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/1485822416/photo/refreshing-passion-fruit-drink-juice-or-cocktail-with-a-reusable-eco-straw.jpg?s=612x612&w=0&k=20&c=TDrJF7s1_yeAzQtBaTNrsyNrc5M6oqFUUdzKX6H4Ies=",
         price: 4.0,
       },
       {
         name: "Peach Green Tea",
         description: "A refreshing drink with peach and green tea flavor.",
-        image: "/images/peach-green-tea-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/588599902/photo/ice-tea.jpg?s=612x612&w=0&k=20&c=8SRBuAI7mXStnGA3PHXlLrRoSUfa3RdPgu3vuB8P-Bs=",
         price: 4.0,
       },
       {
         name: "Strawberry Lemonade",
         description:
           "A refreshing drink with strawberries and lemonade flavor.",
-        image: "/images/strawberry-lemonade-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/1294007127/photo/strawberry-lemonade-vodka.jpg?s=612x612&w=0&k=20&c=h9ItDo3xyAHT43eX4XED-U6nvebI0jaGVqQjMlVwnmk=",
         price: 4.0,
       },
       {
         name: "Cucumber Melon",
         description: "A refreshing drink with cucumber and melon flavor.",
-        image: "/images/cucumber-melon-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/967299356/photo/cucumber-lemonade-in-the-plastic-cup.jpg?s=612x612&w=0&k=20&c=m_2ojcOsyjoNaOto8hXe_EvdddZ4Y4hI1sN_WSBpXoc=",
         price: 4.0,
       },
       {
         name: "Watermelon",
         description: "A refreshing drink with watermelon flavor.",
-        image: "/images/watermelon-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/674180414/photo/watermelon-smoothie-refresher-tasty-of-summer-the-health-benefits-of-watermelon-smoothie.jpg?s=612x612&w=0&k=20&c=VQA8-DQidPnLfBp6GtNN1ixEDfaWB5mALq7flIXGBTA=",
         price: 4.0,
       },
       {
         name: "Berry Hibiscus",
         description:
           "A refreshing drink with mixed berries and hibiscus flavor.",
-        image: "/images/berry-hibiscus-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/1569029375/photo/the-glass-of-iced-tea-with-berries-on-the-wooden-table.jpg?s=612x612&w=0&k=20&c=oC1EjzwVN-aoXi0SobE0kzSjDIaGgTgDcEdVEfAyheQ=",
         price: 4.0,
       },
       {
         name: "Coconut Lime",
         description: "A tropical drink with coconut and lime flavor.",
-        image: "/images/coconut-lime-refresher.jpg",
+        image:
+          "https://media.istockphoto.com/id/1314911454/photo/tropical-caribbean-cocktail-mojito-with-coconut-cream-and-hard-seltzer.jpg?s=612x612&w=0&k=20&c=VwoaFjS2OePl7PpSo-FKFacmspDBl4jcgYE3a8HG624=",
         price: 4.0,
       },
     ],
@@ -435,76 +448,65 @@ export const drinkMenu: MenuCategory[] = [
       {
         name: "Earl Grey Tea",
         description: "A classic black tea with bergamot flavor.",
-        image: "/images/earl-grey-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/1427971802/photo/glass-cup-of-tasty-bergamot-tea-on-wooden-table-space-for-text.jpg?s=612x612&w=0&k=20&c=z6TjWMHtsYpzVwyYz7kMFnUt-yjffb3nWVZ4SbSN6PU=",
         price: 2.5,
       },
       {
         name: "Green Tea",
         description: "A refreshing and healthy green tea.",
-        image: "/images/green-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/1645828793/photo/herbal-tea.jpg?s=612x612&w=0&k=20&c=AHhiPJDgNkBd4cw6BRwM8qKzt7NC3HfkgUfhn5oYvJA=",
         price: 2.5,
       },
       {
         name: "Chamomile Tea",
         description: "A soothing herbal tea with chamomile flowers.",
-        image: "/images/chamomile-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/183758663/photo/chamomile-tea-infusion.jpg?s=612x612&w=0&k=20&c=Gpdg2oKhq8sgqwl7TbMn4mJ5hWrnnem5z5jaVTjO-84=",
         price: 2.5,
       },
       {
         name: "Peppermint Tea",
         description: "A refreshing herbal tea with peppermint leaves.",
-        image: "/images/peppermint-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/1168205037/photo/cup-of-tea-drink-with-fresh-leaves-of-peppermint-melissa-gray-background-healing-herbal-drink.jpg?s=612x612&w=0&k=20&c=A05XYnK9TDPY1AgnmcwP_csPkKbhJX4eD3qGg2bJ7k8=",
         price: 2.5,
       },
       {
         name: "Chai Tea",
         description: "A spiced black tea with milk and spices.",
-        image: "/images/chai-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/1814574844/photo/traditional-indian-masala-chai-latte-in-a-glass-cup-hot-drink-with-milk-spices-and-herbs-on-a.jpg?s=612x612&w=0&k=20&c=6G3D8DplL0MI73O2P0Ye9RbclbCbR-U1aSJR8B54ZL4=",
         price: 2.5,
-      },
-      {
-        name: "Iced Tea",
-        description: "A refreshing iced tea with lemon flavor.",
-        image: "/images/iced-tea.jpg",
-        price: 2.5,
-      },
-      {
-        name: "Matcha Tea",
-        description: "A creamy and earthy matcha green tea.",
-        image: "/images/matcha-tea.jpg",
-        price: 3.0,
       },
       {
         name: "Rooibos Tea",
         description: "A caffeine-free herbal tea with a sweet flavor.",
-        image: "/images/rooibos-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/478180576/photo/healthy-traditional-herbal-rooibos-beverage-tea-with-spices-on-vintage.jpg?s=612x612&w=0&k=20&c=6xjh1zq-AYrboiP3S4rE5gWiFeixEPniwnzvj6XFAMM=",
         price: 2.5,
       },
       {
         name: "Lemon Ginger Tea",
         description: "A refreshing herbal tea with lemon and ginger.",
-        image: "/images/lemon-ginger-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/451066417/photo/ginger-tea-with-honey-and-lemon.jpg?s=612x612&w=0&k=20&c=AP1Ghk3M65wt1J3GhPjJKSYvlOu8TDHJQ5_3Oofl3Sg=",
         price: 2.5,
       },
       {
         name: "Hibiscus Tea",
         description: "A tart and fruity herbal tea with hibiscus flowers.",
-        image: "/images/hibiscus-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/1840883649/photo/cup-of-hot-hibiscus-tea-on-table.jpg?s=612x612&w=0&k=20&c=Ix-ruGwDeqQ0qxyIVPgDoi4piiHs2xdRyzwqZfcypbo=",
         price: 2.5,
       },
       {
         name: "Jasmine Green Tea",
         description: "A fragrant green tea with jasmine flowers.",
-        image: "/images/jasmine-green-tea.jpg",
+        image:
+          "https://media.istockphoto.com/id/94373124/photo/jasmine-tea.jpg?s=612x612&w=0&k=20&c=lfLJpy3PJYlcAHOgEz4i6K5CkvxNuTeINDHc-QisT7w=",
         price: 2.5,
-      },
-
-      {
-        name: "London Fog",
-        description:
-          "A creamy Earl Grey tea latte with vanilla and steamed milk.",
-        image: "/images/london-fog.jpg",
-        price: 3.5,
       },
     ],
   },
@@ -515,186 +517,99 @@ export const drinkMenu: MenuCategory[] = [
       {
         name: "Strawberry Banana",
         description: "A classic smoothie with strawberries and bananas.",
-        image: "/images/strawberry-banana-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/154406583/photo/fresh-fruits-with-bananas-and-smoothies.jpg?s=612x612&w=0&k=20&c=6thXaLgnitbMC8oG5je488M5wC4ZaRozLo24_pNhYWE=",
         price: 4.5,
       },
       {
         name: "Mango Pineapple",
         description: "A tropical smoothie with mango and pineapple.",
-        image: "/images/mango-pineapple-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/2206486433/photo/refreshing-tropical-smoothie-with-mango-and-pineapple-in-modern-kitchen-setting.jpg?s=612x612&w=0&k=20&c=xUjVW37GmpZqXZaUWnS7YyRo9Dr2dKMNT8LPezm7QiM=",
         price: 4.5,
       },
       {
         name: "Berry Blast",
         description: "A mixed berry smoothie with blueberries and raspberries.",
-        image: "/images/berry-blast-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/839497302/photo/sweet-homemade-healthy-berry-smoothie.jpg?s=612x612&w=0&k=20&c=p5PFlYp1Cmkk8Bapp_CaanDcIOtM0IHD2NXfaZ8o3lY=",
         price: 4.5,
       },
       {
         name: "Green Detox",
         description: "A healthy smoothie with spinach, kale, and green apple.",
-        image: "/images/green-detox-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/2167011716/photo/green-healthy-detox-lemon-and-parsley-juice-green-vegetable-smoothie.jpg?s=612x612&w=0&k=20&c=xDnfrlJrMkeW-CStu-pcKy8Fp32BTZSVQASZa0_0n38=",
         price: 4.5,
       },
       {
         name: "Peach Mango",
         description:
           "A refreshing smoothie with peaches and mangoes blended together.",
-        image: "/images/peach-mango-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/2175043500/photo/mango-and-peach-smoothie-with-ice-cubes-served-in-jar-isolated-on-wooden-board-closeup-side.jpg?s=612x612&w=0&k=20&c=vnCaxBaB0Q-UG7mHhhlnQ64bqIeJQkdb0M2kG5muZFc=",
         price: 4.5,
       },
       {
         name: "Chocolate Banana",
         description: "A rich and creamy smoothie with chocolate and banana.",
-        image: "/images/chocolate-banana-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/2040778850/photo/glass-of-banana-smoothie-with-milk-and-chocolate-copy-space.jpg?s=612x612&w=0&k=20&c=RfqifgnjJpfDX_j3i_OTt_qHCiBzCRbDKLQHJ3wzoVk=",
         price: 4.5,
       },
       {
         name: "Tropical Green",
         description:
           "A tropical smoothie with spinach, pineapple, and coconut milk.",
-        image: "/images/tropical-green-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/1614579054/photo/organic-healty-smoothie.jpg?s=612x612&w=0&k=20&c=5Bqy_S4zM1k2uNEXXs4X2Cng7qHW7THxh501_yLqYUQ=",
         price: 4.5,
       },
       {
         name: "Avocado Berry",
         description:
           "A creamy smoothie with avocado, mixed berries, and yogurt.",
-        image: "/images/avocado-berry-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/512354958/photo/avocado-and-spinach-smoothie.jpg?s=612x612&w=0&k=20&c=YOTn8fTsXk0x3ZtOBpWSTdH3zfm3PNPgGEhUj1HlJGE=",
         price: 4.5,
       },
       {
         name: "Coconut Mango",
         description: "A tropical smoothie with coconut milk and fresh mangoes.",
-        image: "/images/coconut-mango-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/1150000171/photo/mango-smoothie-in-a-bottle-with-slices-of-mango-fruit.jpg?s=612x612&w=0&k=20&c=p1R497U-zaSDICs6wNOjS7AoWdMAzmtzb7h9CdKcWBc=",
         price: 4.5,
       },
       {
         name: "Pineapple Coconut",
         description: "A refreshing smoothie with pineapple and coconut milk.",
-        image: "/images/pineapple-coconut-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/1295593100/photo/tropical-smoothie-with-pineapple-banana-coconut-and-orange.jpg?s=612x612&w=0&k=20&c=UuEtT2l0Knaw4d6KrYslogLsAoLacGIEE0CKNXixckI=",
         price: 4.5,
       },
       {
         name: "Chocolate Peanut Butter",
         description:
           "A rich and creamy smoothie with chocolate and peanut butter.",
-        image: "/images/chocolate-peanut-butter-smoothie.jpg",
-        price: 4.5,
-      },
-      {
-        name: "Berry Protein",
-        description:
-          "A protein-packed smoothie with mixed berries and Greek yogurt.",
-        image: "/images/berry-protein-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/1409579518/photo/perfect-iced-cocoa-aka-iced-cacao-stills-with-ice-cubes.jpg?s=612x612&w=0&k=20&c=ZUCCIGQaKSRWd_sPv27wT3msZR0yHwN7ot72jAKjbi8=",
         price: 4.5,
       },
       {
         name: "Mango Matcha",
         description:
           "A refreshing smoothie with mango and matcha blended together.",
-        image: "/images/mango-green-tea-smoothie.jpg",
-        price: 4.5,
-      },
-      {
-        name: "Peach Strawberry",
-        description:
-          "A fruity smoothie with peaches and strawberries blended together.",
-        image: "/images/peach-strawberry-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/2151837371/photo/refreshing-matcha-float-in-modern-glassware.jpg?s=612x612&w=0&k=20&c=e_0B8MRN9qYAAe_CPOOpaquFK12wuFO12DUzXRkdoGw=",
         price: 4.5,
       },
       {
         name: "Raspberry Lemonade",
         description:
           "A refreshing smoothie with raspberries and lemonade flavor.",
-        image: "/images/raspberry-lemonade-smoothie.jpg",
+        image:
+          "https://media.istockphoto.com/id/1312156267/photo/frozen-smoothie-raspberry-sherbet.jpg?s=612x612&w=0&k=20&c=ekO50xNOnDQFf_7YZOM3wwoFPJ1luKfDtY2vu7nRN7o=",
         price: 4.5,
-      },
-      {
-        name: "Blueberry Acai",
-        description:
-          "A fruity smoothie with blueberries and acai blended together.",
-        image: "/images/blueberry-acai-smoothie.jpg",
-        price: 4.5,
-      },
-      {
-        name: "Cherry Lime",
-        description: "A refreshing smoothie with cherries and lime flavor.",
-        image: "/images/cherry-lime-smoothie.jpg",
-        price: 4.5,
-      },
-      {
-        name: "Pineapple Strawberry",
-        description:
-          "A fruity smoothie with pineapple and strawberries blended together.",
-        image: "/images/pineapple-strawberry-smoothie.jpg",
-        price: 4.5,
-      },
-    ],
-  },
-  {
-    name: "Agua Fresca",
-    description: "A refreshing and fruity drink.",
-    items: [
-      {
-        name: "Hibiscus",
-        description: "A refreshing drink made with hibiscus flowers.",
-        image: "/images/hibiscus-agua-fresca.jpg",
-        price: 3.5,
-      },
-      {
-        name: "Cucumber Lime",
-        description: "A refreshing drink made with cucumber and lime.",
-        image: "/images/cucumber-lime-agua-fresca.jpg",
-        price: 3.5,
-      },
-      {
-        name: "Pineapple",
-        description: "A tropical drink made with fresh pineapple.",
-        image: "/images/pineapple-agua-fresca.jpg",
-        price: 3.5,
-      },
-      {
-        name: "Watermelon",
-        description: "A refreshing drink made with fresh watermelon.",
-        image: "/images/watermelon-agua-fresca.jpg",
-        price: 3.5,
-      },
-      {
-        name: "Mango",
-        description: "A sweet and fruity drink made with fresh mango.",
-        image: "/images/mango-agua-fresca.jpg",
-        price: 3.5,
-      },
-    ],
-  },
-  {
-    name: "Hot Chocolate",
-    description: "A rich and creamy chocolate drink.",
-    items: [
-      {
-        name: "Classic Hot Chocolate",
-        description: "A rich and creamy classic hot chocolate.",
-        image: "/images/classic-hot-chocolate.jpg",
-        price: 3.5,
-      },
-      {
-        name: "Peppermint Hot Chocolate",
-        description: "A festive hot chocolate with peppermint flavor.",
-        image: "/images/peppermint-hot-chocolate.jpg",
-        price: 3.5,
-      },
-      {
-        name: "Mexican Hot Chocolate",
-        description: "A spiced hot chocolate with cinnamon and chili powder.",
-        image: "/images/mexican-hot-chocolate.jpg",
-        price: 3.5,
-      },
-      {
-        name: "White Hot Chocolate",
-        description: "A creamy and sweet white hot chocolate.",
-        image: "/images/white-hot-chocolate.jpg",
-        price: 3.5,
       },
     ],
   },
