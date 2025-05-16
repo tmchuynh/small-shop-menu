9789a522d586d8d5608bdef198f8094256891523
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 00:43:35 -0700

    feat: enhance GalleryPage layout with MasonryGridGallery components

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,11 +1,15 @@
+import { MasonryGridGallery } from "@/components/images/MasonryGridGallery";
+import {
+  masonryGalleryImages1,
+  masonryGalleryImages2,
+} from "@/lib/constants/images/gallery";
+
 export default function GalleryPage() {
   return (
-    <div>
-      <div className="relative">
-        <div>
-          <h2>Gallery</h2>
-        </div>
-      </div>
+    <div className="relative mx-auto w-11/12">
+      <h2>Gallery</h2>
+      <MasonryGridGallery data={masonryGalleryImages1} />
+      <MasonryGridGallery data={masonryGalleryImages2} />
     </div>
   );
 }
