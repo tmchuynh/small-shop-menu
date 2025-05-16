e1162cd94177ff3e5a404e4ea6de1de2eac2ae42
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 16:54:14 -0700

    fix: remove unused tabbedImages constant to clean up code

--- src/lib/constants/images/gallery.ts
+++ src/lib/constants/images/gallery.ts
@@ -27,39 +27,3 @@ export const masonryGalleryImages2 = [
   "https://cdn.pixabay.com/photo/2014/12/11/02/57/coffee-563800_1280.jpg",
   "https://cdn.pixabay.com/photo/2019/03/13/16/41/coffee-4053245_1280.jpg",
 ];
-
-export const tabbedImages = [
-  {
-    label: "Exterior",
-    value: "exterior",
-    images: [
-      "https://media.istockphoto.com/id/2183246664/photo/modern-glass-facade-of-contemporary-office-building-with-outdoor-seating.jpg?s=612x612&w=0&k=20&c=Zu0GouRI7wTfneXkqW9UE8yXM0Xb3ckzWNot-O1z2Bc=",
-      "https://media.istockphoto.com/id/2183247162/photo/modern-outdoor-patio-with-greenery-and-contemporary-design-elements.jpg?s=612x612&w=0&k=20&c=J2XJz8JIA7x0u4RepeKHf__qU7mCM6NvmpwJkQHoSZU=",
-    ],
-  },
-  {
-    label: "Interior",
-    value: "interior",
-    images: [
-      "https://media.istockphoto.com/id/2149830144/photo/3d-render-of-modern-cafe-restaurant.jpg?s=612x612&w=0&k=20&c=lrRcBQZx49_uyZe_zEKXfO54hxqwH7a3pExwP8mqQqE=",
-      "https://media.istockphoto.com/id/1491595178/photo/vintage-style-coffee-shop-interior-3d-rendering.jpg?s=612x612&w=0&k=20&c=y-H5OhxuG0Qd1j0isgQKmTGU9p3k91ClwTrkHmhKHCM=",
-      "https://media.istockphoto.com/id/1143934437/photo/3d-render-of-cafetaria-and-restaurant.jpg?s=612x612&w=0&k=20&c=W8lHiQg_ujRmhK7GWGe4N1ozhV6ZjeN2fFMxT_7uXlk=",
-      "https://media.istockphoto.com/id/1490331569/photo/coffee-shop-interior-3d-rendering.jpg?s=612x612&w=0&k=20&c=a3xzME0KcHGd28Gl3ADOp5Jd-d43hHlYn3GSmG6Nh8M=",
-    ],
-  },
-  {
-    label: "Behind the Scenes",
-    value: "behind-the-scenes",
-    images: [
-      "https://media.istockphoto.com/id/1339743849/photo/over-the-shoulder-asian-chinese-male-barista-pouring-froth-milk-on-coffee-cup-prepared-coffee.jpg?s=612x612&w=0&k=20&c=QQkfYycgXGgICpF8a4tETUaTGEj1Z-n8aSaFrQa52lU=",
-      "https://media.istockphoto.com/id/1366672341/photo/glass-with-ice-and-coffee.jpg?s=612x612&w=0&k=20&c=lR7zj__gNDmO0aXfjV8N9Ti-WnOx2iuNxRRyEDvFCi4=",
-      "https://media.istockphoto.com/id/856004690/photo/making-coffee.jpg?s=612x612&w=0&k=20&c=x43qd-aYQHh4cppefEEoznG5g5qGnNuGx1h-qE8CW1o=",
-      "https://media.istockphoto.com/id/1030882758/photo/espresso-coffee-and-machine.jpg?s=612x612&w=0&k=20&c=k6ODdihU-Q7zG3ftft-pVYIkBf_V5I8V98UQDG3ChzE=",
-      "https://media.istockphoto.com/id/626522696/photo/making-of-cafe-latte-art.jpg?s=612x612&w=0&k=20&c=NHJxYrjzuuAMLLmmb813EC5H8wPerDgoYAuldPyyuTw=",
-      "https://media.istockphoto.com/id/905029034/photo/coffee-dripping-from-the-machine-into-the-cup.jpg?s=612x612&w=0&k=20&c=YwLnvQsss1Q4QFTSxknGxelO-MCYAc32osP8qPFwG-o=",
-      "https://media.istockphoto.com/id/1342611877/photo/close-up-of-hand-pouring-coffee-latte-art.jpg?s=612x612&w=0&k=20&c=lhkQmJDW1DhiiVyB1rNvCvdwTlA-M-t_skexAIPXxc4=",
-      "https://media.istockphoto.com/id/1294131797/photo/fresh-ground-coffee-close-up-back-lit-background.jpg?s=612x612&w=0&k=20&c=OhN64dcZ65RfEhGI7VegSeDRfMyqRyfEqJoNRUTDC7M=",
-      "https://media.istockphoto.com/id/1144838255/photo/barista-pours-milk-into-cold-brew-coffee.jpg?s=612x612&w=0&k=20&c=rGNAA8ZF1cAEbQbfF0kcEDHx_icXXY7iPXFLzPgSrLs=",
-    ],
-  },
-];
